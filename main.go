7d5aa4ab947a26ab13db67889f8a634671522334
egorklgn <egorklgnme@gmail.com>
2025-05-30 11:19:36 +0300

    add function get user input and calculation currency

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
 	const USDtoEUR = 0.89
 	const USDtoRUB = 79.6
 	const EURtoRUB = USDtoRUB / USDtoEUR
+}
+
+func getUserInput() (string, float64, string) {
+	var originalCurrency string
+	var amountForConversion float64
+	var targetСurrency string
+	fmt.Print("Enter the original currency: ")
+	fmt.Print("Enter the amount to convert: ")
+	fmt.Print("Enter the target currency: ")
+	fmt.Scan(&originalCurrency)
+	fmt.Scan(&amountForConversion)
+	fmt.Scan(&targetСurrency)
+	return originalCurrency, amountForConversion, targetСurrency
+}
+
+func calculateCurrency(originalCurrency string, amountForConversion float64, targetCurrency string) float64 {
+
 }
